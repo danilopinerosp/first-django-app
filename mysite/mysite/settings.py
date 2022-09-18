d7101943854cbe884d520bab317cb8cf162a2e4f
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-09-18 17:58:58 -0500

    App app2 to installed app in mysite project

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "app2.apps.App2Config",
     "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
