4a47fc520bb17010e22ca5d5a163e5300d89494e
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-09-18 17:20:26 -0500

    Include firts view into URLs config

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,4 @@
-from http.client import HTTPResponse
 from django.http import HttpResponse
 
 def index(request):
-    return HTTPResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
