8dce85148e977b613b897d4cda3b872cb6c58719
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-09-18 17:26:40 -0500

    Add second app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
+    path("app2/", include("app2.urls")),
     path("admin/", admin.site.urls)
 ]
