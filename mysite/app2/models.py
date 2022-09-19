be0bafba4e66c5df11b1b3bc269caf38cbcb811f
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-09-18 19:59:40 -0500

    Fix typo on class name at app2.models

--- mysite/app2/models.py
+++ mysite/app2/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-class Authors(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     born = models.DateField()
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
-    author = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     genrej = models.CharField(max_length=200)
