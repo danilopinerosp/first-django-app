f87b3046896d7f26657ca85e869a227d5fde1eff
Danilo Piñeros <danilo_pineros@citecsolutions.com>
2022-09-18 20:06:30 -0500

    Add __str__ method to Author class

--- mysite/app2/models.py
+++ mysite/app2/models.py
@@ -5,6 +5,9 @@ class Author(models.Model):
     country = models.CharField(max_length=200)
     born = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     name = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
