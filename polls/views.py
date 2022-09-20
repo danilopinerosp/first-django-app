4123d9e51db2bc0d1b79f454d6526f815144532a
Danilo Piñeros <dspinerosp@gmail.com>
2022-09-19 20:02:30 -0500

    Add type in view's parameters

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,15 @@
+from ctypes.wintypes import HRSRC
 from django.http import HttpResponse
 
-def detail(request, question_id):
+def detail(request: HttpResponse, question_id: int):
     return HttpResponse(f"You're looking at question {question_id}")
 
-def results(request, question_id):
+def results(request: HttpResponse, question_id: int):
     response = f"You're looking at the results of question {question_id}"
     return HttpResponse(response)
 
-def vote(request, question_id):
+def vote(request: HttpResponse, question_id: int):
     return HttpResponse(f"You're voting on question {question_id}")
 
-def index(request):
+def index(request: HttpResponse):
     return HttpResponse("Hello, world. You're at the polls index.")
