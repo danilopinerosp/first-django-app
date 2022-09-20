e9daab67fa2f250d872eb82bc5a05bcf91074311
Danilo Piñeros <dspinerosp@gmail.com>
2022-09-19 20:24:46 -0500

    Refactor index view returning render directly

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-from django.template import loader
+from django.shortcuts import render
 
 
 def detail(request: HttpResponse, question_id: int):
@@ -15,8 +15,7 @@ def vote(request: HttpResponse, question_id: int):
 
 def index(request: HttpResponse):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render()
